8b9648d1f4ac9326fd5ed20c16847280ac8ef504
gustavo-gh <129233444+gustavo-gh@users.noreply.github.com>
2024-10-13 19:41:23 -0300

    MODULOSNPM

---
+++ MODULOSNPM/bkp/index.js
@@ -0,0 +1,20 @@
+ * COM OS SEGUINTES ATRIBUTOS: NOME/SOBRENOME
+ * CRIE UMA FUNÇÃO QUE ITERE SOBRE O ARRAY
+ * E RETIRE NOMES E SOBRENOMES
+ * CRIE UMA VARIAVEL COM O CONTEUDO:
+ *       -> const emailBase = "@escola.pr.gov.br"
+ * A PARTIR DESSES DADOS FORME UM @escola com
+ * NOME,SOBRENOME + EMAILBASE
+ * EXECUTE A FUNÇÃO...
+ */
+
+const pessoas = [
+  {
+    nome: "Bora",
+    sobrenome: "Bil",
+  },
+  {
+    nome: "Gabriel",
+    sobrenome: "Barbosa",
+  },
+];
