e4bf1e5c74f36c2fbe6c85adf163cdfdf04baf26
gustavo-gh <129233444+gustavo-gh@users.noreply.github.com>
2024-10-13 19:40:30 -0300

    loopWhile

---
+++ loopWhile/index.js
@@ -0,0 +1,13 @@
+let n = 1;
+
+while(true) {
+  let n2 = (Math.random() * 60).toFixed(2)
+  console.log(n2)
+}
+
+// while (n <= 10){
+//   //LOOP INFINITO
+//   //CTRL + C  PRA INTERROMPER
+//   console.log(n);
+//   n++;
+// }
