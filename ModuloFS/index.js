347c2294b1d5a3a84f8f93c6a4a505243c13b019
gustavo-gh <129233444+gustavo-gh@users.noreply.github.com>
2024-10-16 20:29:36 -0300

    ModuloFS

---
+++ ModuloFS/index.js
@@ -0,0 +1,22 @@
+// require(fs) // require(http)
+// CRIAR UM SERVIDOR WEB PARA INTERPRETAR O HTML
+
+const fs = require('node:fs')
+const http = require('node:http');
+
+const PORT = 3000;
+const servidor = http.createServer(function (req, res) {
+  fs.readFile("./docs/index.html", "utf-8", function(err, arquivo) {
+    if (err) {
+      console.error("Errou!...: " + err);
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(arquivo);
+      res.end();
+    }
+  });
+});
+servidor.listen(PORT, function () {
+  console.log("Servidor rodando na porta: " + PORT);
+});
+// console.log(fs, http)
